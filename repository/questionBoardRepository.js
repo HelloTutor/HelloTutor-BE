fef7da9cda99de70856fb4e429cf562cb4d71d5e
moonhuicheol <moonhuicheol@gmail.com>
2024-01-13 19:42:28 +0900

    questionBaord 기능구현

--- repository/questionBoardRepository.js
+++ repository/questionBoardRepository.js
@@ -1,11 +1,29 @@
 const connection = require("../db/connection");
 const query = require("../db/query.json");
 
-async function insertQuestionBoard(token, body) {
+async function insertQuestionBoard(token, subject, body) {
     let conn;
     try {
         conn = await connection();
-        const [row] = await conn.execute(query.questionBoard.insert, [token.id, body.title, body.content]);
+        const [row] = await conn.execute(query.questionBoard.insert, [token.id, subject, body.title, body.content]);
+
+        return row;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    } finally {
+        if(conn) conn.release();
+    }
+}
+
+async function selectQuestionBoard(postId, subject) {
+    let conn;
+    try {
+        conn = await connection();
+        await conn.beginTransaction();
+        await conn.execute(query.questionBoard.updateViews, [postId, subject]);
+        const [[row]] = await conn.execute(query.questionBoard.select, [postId, subject]);
+        await conn.commit();
 
         return row;
     } catch(error) {
@@ -15,6 +33,66 @@ async function insertQuestionBoard(token, body) {
     }
 }
 
+async function updateQuestionBoard(body, postId, subject, token) {
+    let conn;
+    try {
+        conn = await connection();
+        const [row] = await conn.execute(query.questionBoard.update, [body.title, body.content, postId, subject, token.id]);
+
+        return row;
+    } catch(error) {
+        throw error;
+    } finally {
+        if(conn) conn.release();
+    }
+}
+
+async function deleteQuestionBoard(postId, subject, token) {
+    let conn;
+    try {
+        conn = await connection();
+        const [row] = await conn.execute(query.questionBoard.delete, [postId, subject, token.id]);
+
+        return row;
+    } catch(error) {
+        throw error;
+    } finally {
+        if(conn) conn.release();
+    }
+}
+
+async function selectAllQuestionBoard(subject, offset, limit){
+    let conn;
+    try {
+        conn = await connection();
+        const [row] = await conn.execute(query.questionBoard.selectAll, [subject, offset, limit]);
+
+        return row;
+    } catch(error) {
+        throw error;
+    } finally {
+        if(conn) conn.release();
+    }
+}
+
+async function selectSearchQuestionBoard(subject, search, offset, limit) {
+    let conn;
+    try{
+        conn = await connection();
+        const[row] = await conn.execute(query.questionBoard.selectSearch, [subject, search, offset, limit]);
+
+        return row;
+    } catch(error) {
+        throw error;
+    } finally {
+        if(conn) conn.release();
+    }
+}
 module.exports = {
     insertQuestionBoard,
+    selectQuestionBoard,
+    updateQuestionBoard,
+    deleteQuestionBoard,
+    selectAllQuestionBoard,
+    selectSearchQuestionBoard
 }
--- router/questionBoard.js
+++ router/questionBoard.js
@@ -1,5 +1,15 @@
 const router  = require("express").Router({mergeParams: true});
 const authorization = require("../middleware/authorization");
+const questionBoardController = require("../controller/question_Board.controller");
 
-router.post("/")
-module.exports = router();
+router.post("/:subject", authorization.reIssueToken, questionBoardController.insertQuestionBoard);
+
+router.get("/:subject", questionBoardController.selectAllQuestionBoard);
+
+router.get("/:subject/:postId", questionBoardController.selectQuestionBoard);
+
+router.put("/:subject/:postId", authorization.reIssueToken, questionBoardController.putQuestionBoard);
+
+router.delete("/:subject/:postId", authorization.reIssueToken, questionBoardController.deleteQuestionBoard);
+
+module.exports = router;
