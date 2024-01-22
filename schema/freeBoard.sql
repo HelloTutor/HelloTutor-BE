02188d2f42d3fe76572b1c694440d39ef59be6ba
moonhuicheol <moonhuicheol@gmail.com>
2024-01-22 22:01:21 +0900

    sql파일 수정

--- schema/freeBoard.sql
+++ schema/freeBoard.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `tb_free_board` (
-	`id` int NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`user_id` int NOT NULL,
 	`title` varchar(50) DEFAULT NULL,
 	`content` mediumtext,
@@ -7,8 +7,5 @@ CREATE TABLE `tb_free_board` (
 	`updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
 	`views` int DEFAULT '0',
 	`content_json` json NOT NULL,
-	PRIMARY KEY (`id`),
-	KEY `fk_question_board_user_id` (`user_id`),
-	CONSTRAINT `fk_free_board_user_id` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`) ON DELETE CASCADE,
-	CONSTRAINT `fk_question_board_user_id` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`) ON DELETE CASCADE
-);
+	CONSTRAINT `fk_free_board_user_id` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`) ON DELETE CASCADE
+)
