1e90b8a30f9a07ce297dc48e4e79ea26eeb4c712
moonhuicheol <moonhuicheol@gmail.com>
2024-01-26 23:26:03 +0900

    sql 수정

--- schema/like.sql
+++ schema/like.sql
@@ -4,5 +4,5 @@ CREATE TABLE `tb_like` (
     `free_board_comments_id` int DEFAULT NULL,
     CONSTRAINT `fk_like_free_board_comments_id` FOREIGN KEY (`free_board_comments_id`) REFERENCES `tb_free_board_comments` (`id`) ON DELETE CASCADE,
     CONSTRAINT `fk_like_free_board_id` FOREIGN KEY (`free_board_id`) REFERENCES `tb_free_board` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_like_user_id` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`)
+    CONSTRAINT `fk_like_user_id` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`) ON DELETE CASCADE
 );
